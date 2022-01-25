1a347d5f49a82916c65e38e327557dbb96b91e8a
Jackson Morris-Thring <jacksonmorris-thring@Jacksons-Air.lan>
2022-01-25 12:04:13 +1030

    adding in variable based server fetch request to allow for function wrapping in weather.js

--- assets/js/Heightscript.js
+++ assets/js/Heightscript.js
@@ -1,32 +1,25 @@
 
-var requestURL = "https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536%2C-104.9847034&key=AIzaSyBzLHUoT5HXQ28s19821jaSJxj1QBHXhpc";
+//Hard coded latt and long variables. Note: sig figs are flexible, can many or few
+//decimal points
+var lattVar = 39;
+var longVar = -104;
 
-fetch(requestURL)
+//Request URL with location query paramter and api key paramter
+var fnRequestURL = "https://maps.googleapis.com/maps/api/elevation/json?locations="+lattVar+"%2C"+longVar+"&key=AIzaSyBzLHUoT5HXQ28s19821jaSJxj1QBHXhpc";
+
+
+fetch(fnRequestURL)
     .then (function(response){
-        console.log(response.status);
+        // console.log(response.status);
         return response.json();
     })
 
     .then( function(data){
-        console.log("returned data is: ");
-        console.log(data);
-        console.log("returned data.results is: ");
-        console.log(data.results);
-        console.log("returned data.results[0] is: ");
-        console.log(data.results[0]);
+        //Debugging showing path
+        // console.log("returned data.results[0] is: ");
+        // console.log(data.results[0]);
 
-
+        //assigning an object variable to the google data object containing elevation
         var objectData = data.results[0]
         console.log("returned data converted from object is: " + objectData.elevation);
     })
-
-    
-
-
-    // const elevator = new google.maps.ElevationService();
-
-    // elevator
-    // .getElevationForLocations({
-    //   locations: [39.7391536,104.9847034],
-    // })
-    // .then(({ results }) => {
